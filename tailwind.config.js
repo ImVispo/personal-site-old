f9456f05e7ae4c85a3ef961a16d12df37b2d2758
ImVispo <nickcboboaca@gmail.com>
2022-01-04 22:42:08 -0800

    tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+const defaults = require('tailwindcss/defaultTheme');
+
+/** @type {import("@types/tailwindcss/tailwind-config").TailwindConfig } */
+module.exports = {
+	content: ['./src/**/*.{tsx,ts,css}'],
+	darkMode: 'media',
+	theme: {
+		extend: {
+			fontFamily: {
+				...defaults.fontFamily,
+				sans: ['Roboto', ...defaults.fontFamily.sans],
+			},
+		},
+	},
+	variants: {
+		typography: ['dark'],
+		animation: ['motion-safe'],
+	},
+	plugins: [require('@tailwindcss/typography')],
+};
