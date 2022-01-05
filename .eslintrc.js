debb7879b205c55c5bc42f1a70573118dc167827
ImVispo <nickcboboaca@gmail.com>
2022-01-04 22:04:41 -0800

    prettier, src

---
+++ .eslintrc.js
@@ -0,0 +1,50 @@
+
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: [
+		'next',
+		'next/core-web-vitals',
+		'plugin:react/recommended',
+		'xo',
+		'xo-typescript',
+		'xo-react',
+		'plugin:tailwindcss/recommended',
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaFeatures: {jsx: true},
+		ecmaVersion: 12,
+		sourceType: 'module',
+		project: './tsconfig.json',
+	},
+	plugins: ['react', '@typescript-eslint', 'tailwindcss'],
+	ignorePatterns: ['**/*.js'],
+	rules: {
+		// Next.js
+		'@typescript-eslint/triple-slash-reference': 'off',
+		'react/react-in-jsx-scope': 'off',
+
+		// Prettier handles these
+		'@typescript-eslint/comma-dangle': 'off',
+		'react/function-component-definition': 'off',
+		'react/jsx-tag-spacing': 'off',
+		'react/no-unescaped-entities': 'off',
+		'no-mixed-operators': 'off',
+		'operator-linebreak': 'off',
+		'@typescript-eslint/naming-convention': 'off',
+		'quote-props': 'off',
+		'@typescript-eslint/quotes': 'off',
+		'react/jsx-curly-newline': 'off',
+		'@typescript-eslint/indent': 'off',
+
+		// Bruh
+		'@typescript-eslint/ban-types': 'off',
+
+		// Doesn't work with 3.0.0 jit
+		'tailwindcss/no-custom-classname': 'off',
+	},
+};
